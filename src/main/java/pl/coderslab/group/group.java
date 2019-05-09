5757a54c1c0681a03e6fbd02d47cbbc4a354e7a2
krzysiektom <krzysiektom@op.pl>
2019-05-09 07:55:08 +0200

    group

---
+++ src/main/java/pl/coderslab/group/group.java
@@ -0,0 +1,47 @@
+package pl.coderslab.group;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "groups")
+public class group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String abbr;
+
+    public group() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbr() {
+        return abbr;
+    }
+
+    public void setAbbr(String abbr) {
+        this.abbr = abbr;
+    }
+}
